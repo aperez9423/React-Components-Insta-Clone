12511b4c796613249ef105946efd1f41601e8687
Angelica Perez <aperez9423@gmail.com>
2020-06-19 20:19:02 -0400

    Completed comments and card sections.

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,14 +5,17 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
-  // Add state for the comments
+  const [data, setData] = useState(props.comments);
 
   return (
-    <div>
-      {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+    <div> 
+      {data.map (p => ( 
+        <Comment key = {p.id} comment = {p} />
+      ))}   
+    <CommentInput />
     </div>
-  );
+  )
 };
+
 
 export default CommentSection;
